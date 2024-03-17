14a50875c00fe0b2c1187bd2674ebbe9271496de
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-03-17 17:27:23 +0100

    feat: add dummy presentations

---
+++ src/presentations/postgresql-advanced-features/slides.js
@@ -0,0 +1,15 @@
+export default function Content(){
+  return (
+    <div className="slides">
+      <section>
+        <h1>
+          PostgteSQL Advanced Features
+        </h1>
+      </section>
+      <section>
+        <section>Slide 2</section>
+        <section>Slide 3</section>
+      </section>
+    </div>
+  )
+}
