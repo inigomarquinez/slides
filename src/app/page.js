4d7c375bc195699ac990afb797e8eeb3001a9878
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-03-17 18:21:13 +0100

    refactor: improve home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,28 @@
-import presentations from "@/presentations";
+import path from 'path';
 
-export default function Home() {
+import readSubfolders from "@/utils/readSubfolders";
+import PresentationGrid from '@/components/presentationGrid';
+
+async function getPresentations() {
+  const folders = readSubfolders(path.join(process.cwd(), 'src', 'presentations'));
+
+  const presentations = await Promise.all(folders.map(async (folder) => {
+    const metadata = await (await import(`@/presentations/${folder.name}/metadata.js`)).default;
+    return {
+      ...metadata,
+      ...folder
+    };
+  }));
+
+  return presentations;
+}
+
+export default async function Home() {
+  const presentations = await getPresentations();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {presentations.map((presentation) => (
-        <div
-          key={presentation.id}
-          className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <a
-            href={`/slides/${presentation.id}`}
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              {presentation.title}{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              {presentation.description}
-            </p>
-          </a>
-        </div>
-      ))}
+      <PresentationGrid presentations={presentations} />
     </main>
   );
 }
